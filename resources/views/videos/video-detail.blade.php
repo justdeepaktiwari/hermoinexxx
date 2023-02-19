928b4629afde82bf009334abc8cf12625b7868dc
Deepak Tiwari <deepurajpandit2@gmail.com>
2023-02-20 02:58:23 +0530

    autoplay added

--- resources/views/videos/video-detail.blade.php
+++ resources/views/videos/video-detail.blade.php
@@ -65,7 +65,7 @@
                             $type = explode(".", $video_detail->video_url);
                             $type = isset($type[count($type)-1]) ? $type[count($type)-1] : "mp4";
                         @endphp
-                        <video class="video" controls>
+                        <video class="video" controls muted autoplay>
                             <source src="{{ $video_detail->video_url }}" type="video/{{ $type }}">
                         </video>
                     </div>
